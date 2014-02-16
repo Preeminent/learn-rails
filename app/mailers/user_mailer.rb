3ef2730d6a8f8fa45f393bc2f1a620894892a234
Anthony <anthony@preeminentproductions.com>
2014-02-16 14:48:14 -0800

    Send Mail

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,8 @@
+class UserMailer < ActionMailer::Base
+  default from: "do-not-reply@test.com"
+
+  def contact_email(contact)
+  	 @contact = contact
+  	 mail(to: ENV["OWNER_EMAIL"], from: @contact.email, :subject => "Website Contact Form")
+  	end
+end
